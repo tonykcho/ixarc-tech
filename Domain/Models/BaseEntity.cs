cb3c065981e5bef30453af609c2886d3ffe38f74
Tonyho <tonykcho3527657@gmail.com>
2020-11-26 15:56:44 +0800

    Init Domain

---
+++ Domain/Models/BaseEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain.Models
+{
+    public abstract class BaseEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Guid { get; set; }
+    }
+}
---
+++ Domain/Models/Identities/AppUser.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace Domain.Models.Identities
+{
+    public class AppUser : BaseEntity
+    {
+        public AppUser() { }
+    }
+}
